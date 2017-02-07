cb992bfff5ab255da2933fb860a1d8bc9a2e9863
proj2k <proj2k@gmail.com>
2017-02-07 18:17:45 +0900

    test

---
+++ src/SaveDiagram.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by kimjaesu on 2017. 2. 7..
+ */
+public class SaveDiagram extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
